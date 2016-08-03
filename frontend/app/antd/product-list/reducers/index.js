a31c2367df7c5d683b974d77e40cf05db1583e2c
zhangbaitong <zhangbaitong@163.com>
2016-08-03 16:21:16 +0800

    refactor reducers by split two state source

--- frontend/app/antd/product-list/reducers/index.js
+++ frontend/app/antd/product-list/reducers/index.js
@@ -3,90 +3,90 @@ import { combineReducers } from 'redux'
 import * as types from '../constants/ActionTypes'
 
 function addSearchProduct(state,item){
-  var tmp = Object.assign({},state)
-  tmp.searchSource = Object.assign({},state.searchSource,item);
-  return tmp
+  return Object.assign({},state,item);
 }
 
-function addProduct(state){
-  if(state.dataSource == undefined){
-    state.dataSource = []
-  }
-  var tmp = Object.assign({},state)
-  tmp.dataSource = [...tmp.dataSource,{key: parseInt(Math.random()*100,10)+1,
+function addProduct(state = []){
+  return [...state,{key: parseInt(Math.random()*100,10)+1,
         name: `商品`,
         num: `122`,
         desc: `网店直供号`}]
-  return tmp
-}
-
-function deleteProduct(state,item){
-  if(state.dataSource == undefined){
-    state.dataSource = []
   }
-  var tmp = Object.assign({},state)
-  tmp.dataSource = [
-        ...state.dataSource.slice(0, item.indexForSort),
-        ...state.dataSource.slice(item.indexForSort + 1)
+
+function deleteProduct(state = [],item){
+  return [
+        ...state.slice(0, item.indexForSort),
+        ...state.slice(item.indexForSort + 1)
       ]
-  return tmp
 }
 
 
-function updateProduct(state,item){
-  if(state.dataSource == undefined){
-    state.dataSource = []
-  }
-  var tmp = Object.assign({},state)
-  tmp.dataSource = [
-        ...state.dataSource.slice(0, item.indexForSort),
+function updateProduct(state = [],item){
+  return [
+        ...state.slice(0, item.indexForSort),
         {key: parseInt(Math.random()*100,10)+1,
           name: `商品999`,
           num: `999`,
           desc: `网店直供999号`,
         },
-        ...state.dataSource.slice(item.indexForSort + 1)
+        ...state.slice(item.indexForSort + 1)
       ]
-  return tmp
 }
 
-function searchProduct(state,item){
-  var searchItem = state.searchSource
+function searchProduct(state = [],searchItem){
+  console.log('>>>>>>>>>>>>>',state,searchItem)
+  var tmp = []
   if(searchItem.name == null && searchItem.desc == null){
-    return Object.assign({},state)
+    return tmp
   }
-  var tmp = Object.assign({},state)
-  console.log(tmp.dataSource)
-  tmp.dataSource = []
-  state.dataSource.map(
+  state.map(
     (item) => {if((searchItem.name != null && item.name.match(searchItem.name) != null) && (searchItem.desc != null &&  item.desc.match(searchItem.desc) != null)){
-      tmp.dataSource.push(item)
+      tmp.push(item)
     }
     return item})
+  console.log('>>>>>>>>>>>>>',tmp,searchItem)
   return tmp
 }
 
-function searchClearProduct(state,item){
-  var tmp = Object.assign({},state)
-  tmp.searchSource = {}
-  return tmp
+function searchClearProduct(state = {},item){
+  return {}
 }
 
-export default function productListReducer(state = [], action) {
-  switch (action.type) {
-    case types.ADD_ITEM:
-      return addProduct(state)
+function searchSource(state = {}, action){
+    switch (action.type) {
     case types.ADD_SEARCH_ITEM:
       return addSearchProduct(state,action.item)
-    case types.UPDATE_ITEM:
-      return updateProduct(state,action.item)
-     case types.DELETE_ITEM:
-      return deleteProduct(state,action.item)
-     case types.SEARCH_ITEM:
-      return searchProduct(state,action.item)
      case types.SEARCH_CLEAR_ITEM:
       return searchClearProduct(state,action.item)
     default:
       return state
   }
 }
+
+function dataSource(state = [], action,searchSource){
+    switch (action.type) {
+    case types.ADD_ITEM:
+      return addProduct(state)
+    case types.UPDATE_ITEM:
+      return updateProduct(state,action.item)
+    case types.DELETE_ITEM:
+      return deleteProduct(state,action.item)
+    case types.SEARCH_ITEM:
+      return searchProduct(state,searchSource)
+    default:
+      return state
+  }
+}
+
+
+export default function productListReducer(state, action) {
+  return {
+    searchSource: searchSource(state.searchSource, action),
+    dataSource: dataSource(state.dataSource, action, state.searchSource)
+  }
+}
+
+// export default combineReducers({
+//     searchSource,
+//     dataSource
+// })
